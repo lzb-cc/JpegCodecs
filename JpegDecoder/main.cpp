93f9f9ab169cbffccff77bcb4068657a732cf5fb
lzb-cc <937907891@qq.com>
2017-07-05 12:37:34 +0800

    Add files via upload

---
+++ JpegDecoder/main.cpp
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include "JpegDecoder.h"
+#include "BmpEncoder.h"
+
+using namespace JpegCodec;
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Usage: main input.jpg out.bmp\n");
+		return 0;
+	}
+
+	JpegDecoder decoder(argv[1]);
+	Matrix mat;
+    	decoder.Decoder(mat);
+
+	/* 保存为 Bitmap 格式图像 */
+	int size;
+	unsigned char *bitmap = Encoder(mat.data, mat.rows, mat.cols, size);
+	Write(argv[2], bitmap, size);
+	return 0;
+}
